2d26411d26c2c7ee58b8fc54860e3bd62a66f6b1
Peter <peterdotliu@gmail.com>
2020-11-26 14:31:03 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3e83d4613b7942f673e5.js"
+    "url": "webpack-runtime-54c9c97cc02000097f02.js"
   },
   {
     "url": "framework-868c53fff4771b0bde98.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "04b422fda77c30cbf089438b2f960e9b"
+    "revision": "fd89cdca301bdddf0c3eab1561ac8562"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "87da5ddb0a42c403564e0867f3f42b70"
+    "revision": "60a4af3ff1a88d9031ed46a2dc392448"
   },
   {
     "url": "manifest.webmanifest",
